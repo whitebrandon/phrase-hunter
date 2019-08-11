ccf5bbfa906ee48d6b8ac477299f32f7a2f7694a
Brandon White <white.brandonsean@gmail.com>
2019-08-11 10:56:52 -0400

    Added brainstorming ideas

---
+++ js/phrases.js
@@ -0,0 +1,79 @@
+const phrase = [
+    {
+        phrase: `Don't judge a book by its cover`,
+        category: `popular idioms`,
+        hint: ``
+    },
+    {
+        phrase: `Buzz Lightyear`,
+        category: `fictional characters`,
+        hint: ``
+    },
+    {
+       phrase: `I think therefore I am`,
+       category: `philisophical quotes`,
+       hint: `Rene Descartes` 
+    },
+    {
+        phrase: `The unexamined life is not worth living`,
+        category: `philisophical quotes`,
+        hint: `Socrates`
+    },
+    {
+        phrase: `There is nothing permanent except change`,
+        category: `philisophical quotes`,
+        hint: `Heraclitus`   
+    },
+    {
+        phrase: `Everything has beauty, but not everyone sees it`,
+        category: `philisophical quotes`,
+        hint: `Confucius`
+    },
+    {
+        phrase: `A friend to all is a friend to none`,
+        category: `philosphical quotes`,
+        hint: `Aristotle`
+    },
+    {
+        phrase: `Be where your enemy is not`,
+        category: `philisophical quotes`,
+        hint: `Sun Tzu`
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+    {
+        
+    },
+
+]
