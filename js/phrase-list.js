94a6109dbfbdbda978855017585bacd17c01e461
Brandon White <white.brandonsean@gmail.com>
2019-08-15 01:18:31 -0400

    Added objects to phrase array

--- js/phrase-list.js
+++ js/phrase-list.js
@@ -1,42 +1,122 @@
 const phrase = [
     {
-        phrase: `Dont judge a book by its cover`,
+        phrase: `Dont judge a book by its cover.`,
         category: `popular idioms`,
         hint: `Sometimes your reading material is better than expected`
     },
     {
-        phrase: `Buzz Lightyear`,
+        phrase: `Buzz Lightyear.`,
         category: `fictional characters`,
         hint: `An action figure with delusions of grandeur`
     },
     {
-       phrase: `I think therefore I am`,
+       phrase: `I think therefore I am.`,
        category: `philisophical quotes`,
        hint: `A quote by Rene Descartes` 
     },
     {
-        phrase: `The unexamined life is not worth living`,
+        phrase: `The unexamined life is not worth living.`,
         category: `philisophical quotes`,
         hint: `A quote by Socrates`
     },
     {
-        phrase: `There is nothing permanent except change`,
+        phrase: `There is nothing permanent except change.`,
         category: `philisophical quotes`,
         hint: `A quote by Heraclitus`   
     },
     {
-        phrase: `Everything has beauty, but not everyone sees it`,
+        phrase: `Everything has beauty, but not everyone sees it.`,
         category: `philisophical quotes`,
         hint: `A quote by Confucius`
     },
     {
-        phrase: `A friend to all is a friend to none`,
+        phrase: `A friend to all is a friend to none.`,
         category: `philosphical quotes`,
         hint: `A quote by Aristotle`
     },
     {
-        phrase: `Be where your enemy is not`,
+        phrase: `Be where your enemy is not.`,
         category: `philisophical quotes`,
         hint: `A quote by Sun Tzu`
+    },
+    {
+        phrase: `Excuse me while I kiss the sky.`,
+        category: `song ♪ lyrics`,
+        hint: `Jimi Hendrix`
+    },
+    {
+        phrase: `You may say I'm a dreamer, but I'm not the only one.`,
+        category: `song ♪ lyrics`,
+        hint: `John Lennon`
+    },
+    {
+        phrase: `I can't help falling in love with you.`,
+        category: `song ♪ lyrics`,
+        hint: `Elvis Presley`
+    },
+    {
+        phrase: `Tonight, we are young.`,
+        category: `song ♪ lyrics`,
+        hint: `Fun`
+    },
+    {
+        phrase: `There ain't no mountain high enough.`,
+        category: `song ♪ lyrics`,
+        hint: `Marvin Gate & Tammi Terrell`
+    },
+    {
+        phrase: `You can't start a fire without a spark.`,
+        category: `song ♪ lyrics`,
+        hint: `Bruce Springsteen`
+    },
+    {
+        phrase: `Climb the ladder to success, escalator style.`,
+        category: `song ♪ lyrics`,
+        hint: `The Notorious B.I.G.`
+    },
+    {
+        phrase: `I'm sorry, Miss Jackson. I am for real.`,
+        category: `song ♪ lyrics`,
+        hint: `Outkast`
+    },
+    {
+        phrase: `The best part about me is I am not you`,
+        category: `song ♪ lyrics`,
+        hint: `Eminem`
+    },
+    {
+        phrase: `Real G's move in silence like lasagna.`,
+        category: `song ♪ lyrics`,
+        hint: `Lil Wayne`
+    },
+    {
+        phrase: `She was more like a beauty queen from a movie scene.`,
+        category: `song ♪ lyrics`,
+        hint: `Michael Jackson`
+    },
+    {
+        phrase: `Diva is a female version of a hustler.`,
+        category: `song ♪ lyrics`,
+        hint: `Beyoncé`
+    },
+    {
+        phrase: `Don't be a hard rock when you really are a gem`,
+        category: `song ♪ lyrics`,
+        hint: `Lauryn Hill`
+    },
+    {
+        phrase: `There's a fire starting in my heart.`,
+        category: `song ♪ lyrics`,
+        hint: `Adele`
+    },
+    {
+        phrase: `Hit me baby one more time`,
+        category: `song ♪ lyrics`,
+        hint: `Britney Spears`
+    },
+    {
+        phrase: `If you wanna be my lover.`,
+        category: `song ♪ lyrics`,
+        hint: `Spice Girls`
     }
 ]
