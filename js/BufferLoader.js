be408f787394b4e6b6dbb54d297bb69ed1048830
Brandon White <white.brandonsean@gmail.com>
2019-08-18 22:01:41 -0400

    Added BufferLoader and audio scripts

---
+++ js/BufferLoader.js
@@ -0,0 +1,43 @@
+
+class BufferLoader {
+    // Load buffer asynchronously
+    constructor (context, urlList, callback) {
+        this.context = context;
+        this.urlList = urlList;
+        this.onload = callback;
+        this.bufferList = [];
+        this.loadCount = 0;
+    }
+    /**
+     * Makes request for sounds
+     * @param {string} url location of audio file
+     * @param {number} index index of audio file
+     */
+    loadBuffer (url, index) {
+        const loader = this;
+        const request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.responseType = "arraybuffer";
+        request.onload = function () {
+            // Asynchronously decode the audio file data in request.response
+            loader.context.decodeAudioData(request.response).then(function(decodedData) {
+                loader.bufferList[index] = decodedData;
+                        if (++loader.loadCount === loader.urlList.length) {
+                            loader.onload(loader.bufferList);
+                        }
+            })
+        }
+        request.onerror = function () {
+            alert('BufferLoader: XHR error');
+        }
+        request.send();
+    }
+    /**
+     * Loads sounds into game
+     */
+    load () {
+        for (let i = 0; i < this.urlList.length; i++) {
+        this.loadBuffer(this.urlList[i], i);
+        }
+    }
+}
--- js/audio.js
+++ js/audio.js
@@ -1,91 +1,58 @@
-/**
- * Find a Pair - a simple CSS + JS game
- * coded by Anatol Merezhanyi @e1r0nd_crg
- * https://www.youtube.com/c/AnatolMerezhanyi
- */
-const audio = (function() {
+const audio = (function () {
     const music = {
-      'menu': null,
-      'gainNodeMenu': null,
-      'game': null,
-      'gainNodeGame': null
+        'background' : 5,
     };
+
     const sounds = {
-      'clickSound': 2,
-      'gameOver': 3,
-      'selectChip': 4,
-      'deselectChip': 5,
+        'correctLetter' : 0,
+        'wrongLetter' : 1,
+        'gameLost' : 2,
+        'gameWon' : 3,
+        'keypress' : 4,
     }
     let context = null;
     let bufferLoader = null;
     let bufferList = [];
-  
-    function init() {
-      // Fix up prefixing
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      context = new AudioContext();
-    
-      bufferLoader = new BufferLoader(
-        context,
-        [
-          'agp-ambient-reality-pad-4.ogg',
-          'planetjazzbass-the-death-of-gagarin.ogg',
-          'finger-snap.ogg',
-          'failure-sound-effect.ogg',
-          'button-sound.ogg',
-          'button-sound-effect.ogg'
-        ],
-        finishedLoading
-      );
-    
-      bufferLoader.load();
+
+    function init () {
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        context = new AudioContext();
+
+        bufferLoader = new BufferLoader(
+            context, 
+            [
+              "../audio/sounds/correct_letter.wav",
+              "../audio/sounds/wrong_letter.wav",
+              "../audio/sounds/game_lost.wav",
+              "../audio/sounds/game_won.wav",
+              "../audio/sounds/keypress.wav",
+              "../audio/music/Oniku Loop2.wav",
+            ], 
+            finishedLoading
+        );
+        
+        bufferLoader.load()
     }
-  
-    function finishedLoading(bufferedList) {
-      bufferList = bufferedList;
-      createAudio();
-      music.menu.start();
-    }
-  
-    function createAudio() {
-      music.menu = context.createBufferSource();
-      music.gainNodeMenu = context.createGain();
-      music.menu.buffer = bufferList[0];
-      music.menu.loop = true;
-      music.gainNodeMenu.gain.setValueAtTime(0.01, context.currentTime);
-      music.menu.connect(music.gainNodeMenu);
-      music.gainNodeMenu.connect(context.destination);
-      music.gainNodeMenu.gain.exponentialRampToValueAtTime(1.0, context.currentTime + 3.0);
-  
-      music.game = context.createBufferSource();
-      music.gainNodeGame = context.createGain();
-      music.game.buffer = bufferList[1];
-      music.game.connect(music.gainNodeGame);
-      music.gainNodeGame.connect(context.destination);
-      music.gainNodeGame.gain.exponentialRampToValueAtTime(1.0, context.currentTime + 3.0);
-    }
-    
-    function capitalize(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-    
-    window.onload = init;
-  
-    return {
-      swapMusic: function(from, to) {
-        music['gainNode' + capitalize(from)].gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 3.0);
-        music[from].stop(context.currentTime + 3.0);
+    function finishedLoading (bufferedList) {
+        bufferList = bufferedList;
         createAudio();
-        music['gainNode' + capitalize(to)].gain.setValueAtTime(0.07, context.currentTime);
-        music[to].start();
-      },
-  
-      playSound: function(sound) {
-        let source = context.createBufferSource();
-        source.buffer = bufferList[sounds[sound]];
-        source.connect(context.destination);
-        source.start();
-      }
+        // music.background.start();
     }
-  }());
-  
+    function createAudio () {
+        music.background = context.createBufferSource();
+        music.background.buffer = bufferList[5];
+        music.background.loop = true;
+        music.background.connect(context.destination);
+    }
+
+    window.onload = init;
+
+    return {
+        playSound: function (sound, option) {
+            let source = context.createBufferSource();
+            source.buffer = bufferList[sounds[sound]];
+            source.connect(context.destination);
+            source[option]();
+        }
+    }
+}());
